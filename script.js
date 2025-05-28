285dd7a2aacc7e65d63f1e7933a37d5da804b79a
simoneries <192712243+simoneries@users.noreply.github.com>
2025-05-28 22:23:20 +0200

    Create the first child div to container

---
+++ script.js
@@ -0,0 +1,7 @@
+//Create a webpage of 16x16 grids.
+//create one div in the container.
+const container = document.querySelector("#container")
+const div = document.createElement("div")
+container.appendChild(div)
+//create a row of 16 divs.
+//create the grid using flexbox.
