f7b7dfa8b67db05f2da600a38bfdb559cdbc9e97
simoneries <192712243+simoneries@users.noreply.github.com>
2025-05-28 23:20:52 +0200

    Create a row attribute The object is then set to flex

--- script.js
+++ script.js
@@ -3,13 +3,19 @@
 const container = document.querySelector("#container")
 let numberOfDiv = 16
 function createRow(numberOfDivs){
+    const row = document.createElement("div")
+    row.setAttribute("id","row")
+    container.appendChild(row)
     for (let i =0;i<numberOfDivs;i++){
         const div = document.createElement("div")
-        container.appendChild(div)
-    }
+        row.appendChild(div)
+    }  
 }
 
+
 createRow(numberOfDiv)
+
+
 
 
 //create a row of 16 divs.
