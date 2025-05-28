49f9536802245d59715328fce330520f9605426c
simoneries <192712243+simoneries@users.noreply.github.com>
2025-05-28 22:50:55 +0200

    Create 16 divs

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 //Create a webpage of 16x16 grids.
 //create one div in the container.
 const container = document.querySelector("#container")
-const div = document.createElement("div")
-container.appendChild(div)
+let numberOfDiv = 16
+function createRow(numberOfDivs){
+    for (let i =0;i<numberOfDivs;i++){
+        const div = document.createElement("div")
+        container.appendChild(div)
+    }
+}
+
+createRow(numberOfDiv)
+
+
 //create a row of 16 divs.
 //create the grid using flexbox.
