2ec866e5357965ee968c063b934740d7e8d5972e
simoneries <192712243+simoneries@users.noreply.github.com>
2025-05-29 22:49:55 +0200

    Change the color as we hover

--- script.js
+++ script.js
@@ -8,12 +8,36 @@ function createRow(numberOfDivs){
     container.appendChild(row)
     for (let i =0;i<numberOfDivs;i++){
         const div = document.createElement("div")
+        div.setAttribute("id","tile")
         row.appendChild(div)
     }  
 }
 
-for(let i =0;i<numberOfDiv;i++){
-    createRow(numberOfDiv)
+function createGrid(){
+    for(let i =0;i<numberOfDiv;i++){
+        createRow(numberOfDiv)
+    }
+}
+let grid = createGrid()
+hoverOnTiles()
+
+//i want to set up a hoover effect on my tiles = when i pass on a tile, it will change color.
+function hoverOnTiles(){
+    //first i need to pass in a query selector to select the div. 
+    //i need to return all the 
+    const tileList=document.querySelectorAll("#tile");
+    const tileArray=[...tileList]
+    //I pass an event listener to each div
+    tileArray.forEach(function(tile){
+        tile.addEventListener("mouseover",e=>changeTileColor(e,"blue"))
+    })
+
 }
 
-//I want to create 16 rows of divs
+function changeTileColor(e,color){
+    e.target.style.backgroundColor=color
+}
+
+
+
+//next i want to set an event listener that will check if i hover on a div.
